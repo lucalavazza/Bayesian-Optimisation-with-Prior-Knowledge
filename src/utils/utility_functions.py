aa687b1ccb847211727e942841efb5cb4171c29c
lucalavazza <l.lavazza@studenti.unibs.it>
2023-04-20 09:09:50 +0200

    fixes, refactoring, testing

---
+++ src/utils/utility_functions.py
@@ -0,0 +1,128 @@
+import numpy as np
+from scipy import stats
+
+def power_list(input_list, exponent):
+    '''Raises each element of the given input_list to the desired exponent'''
+    
+    return_list = []
+    
+    for element in input_list:
+        if element >= 0:
+            raised_element = element**exponent
+        else:
+            raised_element = -(abs(element)**exponent)
+        return_list.append(raised_element)
+        
+    return return_list
+
+def compute_statistics(y_function, standard_deviation):
+    '''Computes all the necessary lists associated to the Normal Distribution'''
+    
+    return_variates = []
+    return_amps = []
+    return_pdfs = []
+    
+    for mean in y_function:
+        return_variates.append(stats.norm.rvs(mean, standard_deviation, 10))
+        
+        amp = np.linspace(mean-5*standard_deviation, mean+5*standard_deviation, 10)
+        return_amps.append(amp)
+        
+        return_pdfs.append(stats.norm.pdf(amp, mean, standard_deviation))
+    
+    return return_variates, return_amps, return_pdfs
+
+def normalise_pdfs(pdfs_list):
+    '''Normalises the PDFs between 0 and 1'''
+    
+    return_normalised_pdfs_list = []
+    
+    for pdf_list in pdfs_list:
+        temp_list = []
+        
+        pdf_min = min(pdf_list)
+        pdf_max = max(pdf_list)
+        
+        for pdf_value in pdf_list:
+            temp_list.append(round((pdf_value-pdf_min)/(pdf_max-pdf_min),2))
+        
+        return_normalised_pdfs_list.append(temp_list)
+        
+    return return_normalised_pdfs_list
+
+def regret_priors(start_prior, N, beta):
+    '''Adds regret'''
+    
+    return_raised_priors = []
+    
+    for i in range(1,N+1):
+        temp = []
+        gamma = beta/i
+        
+        for p_list in start_prior:
+            temp.append(power_list(p_list, gamma))
+            
+        return_raised_priors.append(temp)
+    
+    return return_raised_priors
+
+def predict_optima_regret(iterations, time_steps, regret_priors, normalised_pdfs):
+    '''Computes the prediction for each time-step and each iteration, according to the effect of gamma (regret)'''
+    
+    return_predictions = []
+    
+    for iteration in range(iterations):
+        temp = []
+        
+        for time_step in range(time_steps):
+            if(min(regret_priors[iteration][time_step])+max(regret_priors[iteration][time_step]))<0:
+                optimum = -min(np.multiply(regret_priors[iteration][time_step], normalised_pdfs[time_step]))
+            else:
+                optimum = -max(np.multiply(regret_priors[iteration][time_step], normalised_pdfs[time_step]))
+                              
+            temp.append(optimum)
+                              
+        return_predictions.append(temp)
+                              
+    return return_predictions
+
+def compute_gap_values(T, summary):
+    G_BO=0
+    G_PIBO=0
+    G_DCBO=0
+    G_DCPIBO=0
+    list_gaps=[]
+    summary_list = list(summary.values())
+    for i in range(len(summary_list)):
+        for t in range(T):
+            # print(summary_list[i][t][0])
+            if i==0:
+                G_BO=G_BO+summary_list[i][t][0]
+                if t==2:
+                    list_gaps.append(G_BO/T)
+            elif i==1:
+                G_PIBO=G_PIBO+summary_list[i][t][0]
+                if t==2:
+                    list_gaps.append(G_PIBO/T)
+            elif i==2:
+                G_DCBO=G_DCBO+summary_list[i][t][0]
+                if t==2:
+                    list_gaps.append(G_DCBO/T)
+            elif i==3:
+                G_DCPIBO=G_DCPIBO+summary_list[i][t][0]
+                if t==2:
+                    list_gaps.append(G_DCPIBO/T)
+    return list_gaps
+
+
+def replace_nan(list_with_nans):
+    list_of_not_nans = []
+    for i in range(len(list_with_nans)):
+        if (np.isnan(list_with_nans[i][2])) == False:
+            list_of_not_nans.append(list_with_nans[i][2])
+        
+    replace_value = sum(list_of_not_nans)/len(list_with_nans)
+
+    list_with_nans[np.isnan(list_with_nans)] = replace_value
+
+    return list_with_nans
